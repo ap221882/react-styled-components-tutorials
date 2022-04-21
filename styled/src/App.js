7c5ec8b3d10cd373537c9b32431064a85da25590
Ajay Pathak <ap221882@gmail.com>
2022-04-21 16:57:29 +0530

    basics of styled components

--- styled/src/App.js
+++ styled/src/App.js
@@ -1,9 +1,32 @@
+import styled from 'styled-components';
+import BasicTitle from './components/BasicTitle';
+import { DefaultButton, HipsterButton } from './components/Buttons';
+import { Button } from '@mui/material';
+
+//tagged template literal
+// const BasicTitle = styled.h1`
+//   text-align: center;
+//   text-transform: capitalize;
+// `;
+
+const StyledBtn = styled(Button)`
+  text-transform: capitalize;
+`;
+
 function App() {
   return (
     <div>
-      <div style={{ padding: '2rem', backgroundColor: 'grey' }}>
-        <h1 className='title'>styled components</h1>
-        <button className='btn'>click me</button>
+      <div style={{ padding: '2rem' }}>
+        {/* <h1 className='title'>styled components</h1> */}
+        <BasicTitle special>styled components</BasicTitle>
+        <BasicTitle>styled components</BasicTitle>
+        <DefaultButton>click me</DefaultButton>
+        <HipsterButton>click me</HipsterButton>
+        <div style={{ padding: '2rem' }}>
+          <h2>extend material-ui button</h2>
+          <Button variant='contained'>Hello Woraaald</Button>;
+          <StyledBtn variant='contained'>Hello World</StyledBtn>;
+        </div>
       </div>
     </div>
   );
