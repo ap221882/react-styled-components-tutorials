34022c4f3001a1da14c7b6bfb2d54f93a1cf67ef
Ajay Pathak <ap221882@gmail.com>
2022-04-21 13:31:51 +0530

    Inline and global styles

---
+++ styled/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <div style={{ padding: '2rem', backgroundColor: 'grey' }}>
+        <h1 className='title'>styled components</h1>
+        <button className='btn'>click me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
