c08893167bcce01094b5e61225fa6a7305b87c37
Ajay Pathak <ap221882@gmail.com>
2022-04-22 11:05:37 +0530

    css helper funcn

--- styled/src/App.js
+++ styled/src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import { HipsterButton } from './components/Buttons';
+import { DefaultButton } from './components/Buttons';
 
 const App = () => {
   return (
     <div style={{ padding: '2rem' }}>
-      <HipsterButton>Click me</HipsterButton>
-      <HipsterButton
-        css={`
-          color: red;
-        `}
-        as='a'
-        href='https://www.github.com/ap221882/'
-      >
-        Click me
-      </HipsterButton>
-      <HipsterButton>Click me</HipsterButton>
-      <div
-        css={`
-          color: green;
-        `}
-      >
-        <h2>hello world i use css prop</h2>
-      </div>
+      <DefaultButton>click me</DefaultButton>
+      <DefaultButton large>click me</DefaultButton>
     </div>
   );
 };
--- styled/src/components/Buttons.js
+++ styled/src/components/Buttons.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const DefaultButton = styled.button`
   background-color: rgb(46, 174, 116);
@@ -8,6 +8,18 @@ export const DefaultButton = styled.button`
   cursor: pointer;
   /* text-transform: capitalize; */
   padding: 0.25rem;
+  /* padding: ${({ large }) => large && '2rem'}; */
+  ${({ large }) =>
+    large
+      ? css`
+          padding: 1rem;
+          font-size: 1.5rem;
+        `
+      : css`
+          padding: 0.5rem;
+          font-size: 1rem;
+        `}
+
   display: block;
   width: 200px;
   margin: 1rem auto;
