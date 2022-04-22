6cf428bfe97ef206a60c978c9b4b51421a2ce438
Ajay Pathak <ap221882@gmail.com>
2022-04-22 10:55:05 +0530

    css and macro prop

--- styled/src/App.js
+++ styled/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-
+import styled from 'styled-components/macro';
 import { HipsterButton } from './components/Buttons';
 
 const App = () => {
   return (
     <div style={{ padding: '2rem' }}>
       <HipsterButton>Click me</HipsterButton>
-      <HipsterButton as='a' href='https://www.github.com/ap221882/'>
+      <HipsterButton
+        css={`
+          color: red;
+        `}
+        as='a'
+        href='https://www.github.com/ap221882/'
+      >
         Click me
       </HipsterButton>
       <HipsterButton>Click me</HipsterButton>
+      <div
+        css={`
+          color: green;
+        `}
+      >
+        <h2>hello world i use css prop</h2>
+      </div>
     </div>
   );
 };
